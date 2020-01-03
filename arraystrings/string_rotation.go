8f7a3ead8144fcfa03e0ec234167d601cc06f5fa
hung.han <hung.han@cxagroup.com>
2020-01-03 17:56:40 +0700

    update

--- arraystrings/string_rotation.go
+++ arraystrings/string_rotation.go
@@ -1,5 +1,29 @@
 package arraystrings
 
 func IsStringRotation(s1 string, s2 string) bool {
+	l := len(s1)
+	if l == len(s2) && l > 0 {
+		s1s1 := s1 + s1
+		return isSubstring(s1s1, s2)
+	}
+	return false
+}
+
+// check if s2 is substring of s1
+func isSubstring(s1 string, s2 string) bool {
+	m := len(s2)
+	n := len(s1)
+
+	for i := 0; i < n-m; i++ {
+		j := 0
+		for ; j < m; j++ {
+			if s1[i+j] != s2[j] {
+				break
+			}
+		}
+		if j == m {
+			return true
+		}
+	}
 	return false
 }
--- main.go
+++ main.go
@@ -6,10 +6,5 @@ import (
 )
 
 func main() {
-	log.Println(arraystrings.SetZeroMatrix([][]int{
-		[]int{1, 2, 3, 4},
-		[]int{5, 0, 7, 8},
-		[]int{9, 10, 11, 12},
-		[]int{13, 14, 15, 16},
-	}))
+	log.Println(arraystrings.IsStringRotation("waterbottle", "erbottlewat"))
 }
