69af98f0f2556c6c11666964cc7932744c31fed6
hung.han <hung.han@cxagroup.com>
2020-01-07 17:23:11 +0700

    update

--- linkedlist/return_kth_to_last.go
+++ linkedlist/return_kth_to_last.go
@@ -1,5 +1,56 @@
 package linkedlist
 
-func ReturnKthToLast(node LinkedListNode) LinkedListNode {
+import "log"
+
+func ReturnKthToLast(head LinkedListNode, k int) LinkedListNode {
+	// r := &head
+	// for r != nil {
+	// 	log.Println("con co", r)
+	// 	r = r.Next
+	// }
+	// log.Println(printKthToLast(&head, k))
+	KthNode := returnKthToLastPointer(&head, k)
+	log.Println("con co", KthNode)
+	return *KthNode
+}
+
+func printKthToLast(head *LinkedListNode, k int) int {
+	if head == nil {
+		return 0
+	}
+	index := printKthToLast(head.Next, k) + 1
+	if index == k {
+		log.Println(k, "th to last node is ", head.Val)
+	}
+	return index
+}
+
+func returnKthToLast(head *LinkedListNode, k, i int) *LinkedListNode {
+	if head == nil {
+		return nil
+	}
+	if i == k {
+		return head
+	}
+	i++
+	node := returnKthToLast(head.Next, k, i)
 	return node
+}
+
+func returnKthToLastPointer(head *LinkedListNode, k int) *LinkedListNode {
+	p1 := head
+	p2 := head
+
+	for i := 0; i < k; i++ {
+		if p1 == nil {
+			return nil
+		}
+		p1 = p1.Next
+	}
+
+	for p1 != nil {
+		p1 = p1.Next
+		p2 = p2.Next
+	}
+	return p2
 }
--- main.go
+++ main.go
@@ -10,9 +10,9 @@ func main() {
 	node1 := linkedlist.LinkedListNode{Val: 1, Next: nil}
 	node2 := linkedlist.LinkedListNode{Val: 2, Next: &node1}
 	node3 := linkedlist.LinkedListNode{Val: 3, Next: &node2}
-	node4 := linkedlist.LinkedListNode{Val: 3, Next: &node3}
-	node5 := linkedlist.LinkedListNode{Val: 4, Next: &node4}
-	node6 := linkedlist.LinkedListNode{Val: 5, Next: &node5}
+	node4 := linkedlist.LinkedListNode{Val: 4, Next: &node3}
+	node5 := linkedlist.LinkedListNode{Val: 5, Next: &node4}
+	node6 := linkedlist.LinkedListNode{Val: 6, Next: &node5}
 
-	log.Println(linkedlist.RemoveDup(node6))
+	log.Println(linkedlist.ReturnKthToLast(node6, 5))
 }
