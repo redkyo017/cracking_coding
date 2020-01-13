eef454a051d124acf27f26c546cb6ef9a1a5658c
hung.han <hung.han@cxagroup.com>
2020-01-13 18:24:27 +0700

    update

--- linkedlist/palindrome.go
+++ linkedlist/palindrome.go
@@ -24,7 +24,33 @@ func IsEqual(l1 *LinkedListNode, l2 *LinkedListNode) bool {
 }
 
 func IsPalindrome(node *LinkedListNode) bool {
-	// solution 1: reversed and compare
-	reversed := ReverseAndClone(node)
-	return IsEqual(node, reversed)
+	// // solution 1: reversed and compare
+	// reversed := ReverseAndClone(node)
+	// return IsEqual(node, reversed)
+
+	// solution 2: stack , slow and fast pointer
+	slow := node
+	fast := node
+	stack := []int{}
+
+	for fast != nil && fast.Next != nil {
+		stack = append(stack, slow.Val)
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	if fast != nil {
+		slow = slow.Next
+	}
+
+	for slow != nil {
+		top := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if top != slow.Val {
+			return false
+		}
+		slow = slow.Next
+	}
+
+	return true
 }
