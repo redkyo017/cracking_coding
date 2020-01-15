989b0b653301e4e6e4a8899d843ac4d1eda6ef14
hung.han <hung.han@cxagroup.com>
2020-01-15 18:04:24 +0700

    update

---
+++ linkedlist/loop_detection.go
@@ -0,0 +1,24 @@
+package linkedlist
+
+func FindBeginLoop(head *LinkedListNode) *LinkedListNode {
+	slow := head
+	fast := head
+
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			break
+		}
+	}
+
+	if fast == nil || fast.Next == nil {
+		return nil
+	}
+	slow = head
+	for slow != fast {
+		slow = slow.Next
+		fast = fast.Next
+	}
+	return fast
+}
