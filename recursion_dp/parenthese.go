37c266d82a05af47e3d425b3de53895c60303b9c
hung.han <hung.han@cxagroup.com>
2020-02-28 14:02:36 +0700

    update

--- recursion_dp/parenthese.go
+++ recursion_dp/parenthese.go
@@ -1,23 +1,23 @@
 package recursion_dp
 
+import "log"
+
 func GenerateParenthese(n int) []string {
-	result := SlowGens(n)
-	return *result
+	result := FastGen(n)
+	return result
 }
 
-func SlowGens(remain int) *[]string {
+func SlowGens(remain int) []string {
 	parentheses := []string{}
-
-	if remain < 0 {
+	if remain == 0 {
 		parentheses = append(parentheses, "")
 	} else {
 		prev := SlowGens(remain - 1)
-		for _, str := range *prev {
+		for _, str := range prev {
 			for i := 0; i < len(str); i++ {
 				if string(str[i]) == "(" {
-					left := str[:i]
-					right := str[i:]
-
+					left := str[:i+1]
+					right := str[i+1:]
 					parentheses = append(parentheses, string(left)+"()"+string(right))
 				}
 			}
@@ -25,5 +25,30 @@ func SlowGens(remain int) *[]string {
 		}
 	}
 
-	return &parentheses
+	return parentheses
+}
+
+func FastGen(remain int) []string {
+	parentheses := []string{}
+	char := make([]byte, remain*2)
+	AddParents(&parentheses, remain, remain, char, 0)
+	return parentheses
+}
+
+func AddParents(parentheses *[]string, leftRemain, rightRemain int, char []byte, index int) {
+	if leftRemain < 0 || rightRemain < leftRemain {
+		return
+	}
+	if leftRemain == 0 && rightRemain == 0 {
+		log.Println("con co", string(char))
+		*parentheses = append(*parentheses, string(char))
+	} else {
+		log.Println("con co index", index)
+		char[index] = []byte("(")[0]
+		AddParents(parentheses, leftRemain-1, rightRemain, char, index+1)
+		log.Println("con co left", string(char))
+		char[index] = []byte(")")[0]
+		AddParents(parentheses, leftRemain, rightRemain-1, char, index+1)
+		log.Println("con co right", string(char))
+	}
 }
