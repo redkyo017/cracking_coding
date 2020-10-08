ae0dbe114d777f88b055ca15fd8ac0cf70708966
redkyo017 <redkyo017@gmai.com>
2020-10-08 18:39:29 +0700

    update jump

--- leet_code/jump_game.go
+++ leet_code/jump_game.go
@@ -23,15 +23,38 @@ import "log"
 // 	log.Println("con co be be", jump(arr))
 // }
 
-func JumpII(arr []int) int {
-	list_step := make(map[int][][]int, len(arr))
+
+func JumpII(nums []int) int {
+	if len(nums) <= 1 { 
+		return 0
+	}
+	list_step := make(map[int][][]int, len(nums))
 	list_step[0] = append([][]int{}, []int{0})
 	list_step[1] = append([][]int{}, []int{1})
 	
-	for i := 2; i < len(arr);i++ {
+	for i := 2; i < len(nums);i++ {
 		stepsAtIndex := [][]int{}
-		log.Println("con heo", i, arr[i], stepsAtIndex)
+		// log.Println("con heo", i, nums[i], stepsAtIndex)
+		for j := i-1; j >= 0; j-- {
+			if nums[j] >= i-j {
+				// log.Println("can step", j)
+				stepAtJ := list_step[j]
+				for _,v := range stepAtJ {
+					newStep := append(v, i-j)
+					stepsAtIndex = append(stepsAtIndex, newStep)
+				}
+			}
+		}
+		list_step[i] = stepsAtIndex
 	}
-	log.Println("con co", list_step, len(list_step))
-	return 0
+	stepAtEnd := list_step[len(nums)-1]
+	const MaxUint = ^uint(0)
+	minStep := int(MaxUint >> 1) 
+	for _,v := range stepAtEnd {
+		if len(v) < minStep {
+			minStep = len(v)
+		}
+	}
+	// log.Println("con co", minStep)
+	return minStep
 }
