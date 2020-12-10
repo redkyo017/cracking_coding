923efd8779eb290cdbc65c3e4764827c0488d9c3
redkyo017 <redkyo017@gmai.com>
2020-12-10 18:57:08 +0700

    revert list use recursion

--- src/leet_code_explore/recursion_I.go
+++ src/leet_code_explore/recursion_I.go
@@ -70,3 +70,42 @@ func SwapPairsImplement() {
 		res = res.Next
 	}
 }
+
+func reverseList(head *ListNode) *ListNode {
+	// var newRevertedNode *ListNode
+	// for head != nil {
+	// 	var newNode ListNode
+	// 	newNode.Val = head.Val
+	// 	newNode.Next = newRevertedNode
+	// 	newRevertedNode = &newNode
+	// 	head = head.Next
+	// }
+	// return newRevertedNode
+	if head.Next == nil {
+		return head
+	}
+	var newRevertedNode *ListNode
+	node := reverseList(head.Next)
+	node.Next = head
+
+	newRevertedNode = node
+	return newRevertedNode
+}
+
+func ReverseListEmplement() {
+	node1 := ListNode{1, nil}
+	node2 := ListNode{2, nil}
+	node3 := ListNode{3, nil}
+	node4 := ListNode{4, nil}
+	node5 := ListNode{5, nil}
+	node1.Next = &node2
+	node2.Next = &node3
+	node3.Next = &node4
+	node4.Next = &node5
+
+	node := reverseList(&node1)
+	for node != nil {
+		log.Println("con co", node)
+		node = node.Next
+	}
+}
