cb2e053bd777f8978f18cb550eded910488fe9f6
redkyo017 <redkyo017@gmai.com>
2020-12-16 18:04:28 +0700

    update

--- src/leet_code_explore/recursion_I.go
+++ src/leet_code_explore/recursion_I.go
@@ -368,8 +368,45 @@ func kthGrammar(N int, K int) int {
 	if N == 1 {
 		return 0
 	}
+	if K%2 == 0 {
+		if kthGrammar(N-1, K/2) == 0 {
+			return 1
+		}
+		return 0
+	}
+	return kthGrammar(N-1, (K+1)/2)
 }
 
 func KthGrammarSolution() {
+	log.Println("con meo", kthGrammar(5, 10))
+}
 
+// UNIQUE BINARY SEARCH TREES II
+// Given an integer n, generate all structurally unique BST's (binary search trees) that store values 1 ... n.
+
+// Example:
+// Input: 3
+// Output:
+// [
+//   [1,null,3,2],
+//   [3,2,null,1],
+//   [3,1,null,null,2],
+//   [2,1,3],
+//   [1,null,2,null,3]
+// ]
+// Explanation:
+// The above output corresponds to the 5 unique BST's shown below:
+//    1         3     3      2      1
+//     \       /     /      / \      \
+//      3     2     1      1   3      2
+//     /     /       \                 \
+//    2     1         2                 3
+
+func generateTrees(n int) []*TreeNode {
+	list := []*TreeNode{}
+	if n == 0 {
+		return list
+	}
+
+	return list
 }
