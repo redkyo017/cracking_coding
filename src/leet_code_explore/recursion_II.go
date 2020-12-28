a10dc276141fadd73a55d22f2e52d1443cf38c62
redkyo017 <redkyo017@gmai.com>
2020-12-28 18:02:24 +0700

    update

--- src/leet_code_explore/recursion_II.go
+++ src/leet_code_explore/recursion_II.go
@@ -142,3 +142,24 @@ func SearchMatrixImplement() {
 	}
 	log.Println("search", searchMatrix(matrix, 5))
 }
+
+// BACK TRACKING TEMPLATE
+// def backtrack(candidate):
+//     if find_solution(candidate):
+//         output(candidate)
+//         return
+
+//     # iterate all possible candidates.
+//     for next_candidate in list_of_candidates:
+//         if is_valid(next_candidate):
+//             # try this partial candidate solution
+//             place(next_candidate)
+//             # given the candidate, explore further.
+//             backtrack(next_candidate)
+//             # backtrack
+//             remove(next_candidate)
+
+func totalNQueens(n int) int {
+
+	return 0
+}
